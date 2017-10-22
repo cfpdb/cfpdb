994f2851f9e6b031f538c591f95d30b6c385a382
Noah Watkins <noahwatkins@gmail.com>
2017-10-21 20:11:29 -0700

    server: add apis for target info
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- server.js
+++ server.js
@@ -6,29 +6,51 @@ const loki = require('lokijs')
 const app = express()
 
 const db = new loki()
+
 const calls = db.addCollection('calls')
+const targets = db.addCollection('targets')
 
 const db_dir = "db"
 fs.readdirSync(db_dir).forEach(cname => {
   const cpath = path.join(db_dir, cname)
   if (fs.lstatSync(cpath).isDirectory()) {
+    targets.insert({shortname: cname})
     fs.readdirSync(cpath).forEach(yname => {
       if (yname.endsWith(".yaml")) {
         const ypath = path.join(cpath, yname)
         console.log("loading call: " + ypath)
         const data = fs.readFileSync(ypath)
         const doc = yaml.load(data)
-        doc['call'] = cname
+        doc['target'] = cname
         calls.insert(doc)
       }
     })
   }
 })
 
-app.get('/:call/:year(\\d+)', function (req, res) {
-  const { call, year } = req.params
-  const doc = calls.findOne({ call: call, year: +year })
-  res.send(doc)
+// targets => list all targets
+app.get('/targets', function (req, res) {
+  res.json(targets.find())
+});
+
+// targets/:target => get target
+app.get('/targets/:target', function (req, res) {
+  const { target } = req.params
+  const doc = targets.findOne({ shortname: target })
+  res.json(doc)
+})
+
+// targets/:target/calls => get target calls
+app.get('/targets/:target/calls', function (req, res) {
+  const { target } = req.params
+  const doc = calls.find({ target: target })
+  res.json(doc)
+})
+
+app.get('/targets/:target/:year(\\d+)', function (req, res) {
+  const { target, year } = req.params
+  const doc = calls.findOne({ target: target, year: +year })
+  res.json(doc)
 })
 
 app.listen(3000, function () {
