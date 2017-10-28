fd744d1943f19c82ff839ad2aba48e52b8d08779
Noah Watkins <noahwatkins@gmail.com>
2017-10-28 10:18:38 -0700

    server: validate schemas
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- server.js
+++ server.js
@@ -3,7 +3,93 @@ const path = require('path')
 const yaml = require('js-yaml')
 const express = require('express')
 const loki = require('lokijs')
+const Ajv = require('ajv')
+const assert = require('assert')
 const app = express()
+
+// TODO: handling date https://github.com/epoberezkin/ajv-keywords
+// TODO: in the short term, just do conversions in the reader and figure out how
+// to get ajv to do everything later...
+const schema = {
+  "type": "object",
+  "definitions": {
+    "dates": {
+      "oneOf": [
+        {
+          "type": "string",
+          "format": "date"
+        },
+        {
+          "type": "array",
+          "items": {
+            "type": "string",
+            "format": "date"
+          },
+          "uniqueItems": true,
+          "minItems": 2
+        }
+      ]
+    },
+    "dateSet": {
+      "oneOf": [
+        { "$ref": "#/definitions/dates" },
+        {
+          "type": "object",
+          "properties": {
+            "abstract":     { "$ref": "#/definitions/dates" },
+            "submit":       { "$ref": "#/definitions/dates" },
+            "notification": { "$ref": "#/definitions/dates" },
+            "rebuttal":     { "$ref": "#/definitions/dates" },
+          },
+          "additionalProperties": false
+        },
+      ]
+    }
+  },
+  "properties": {
+    "year": {
+      "type": "integer"
+    },
+    "dates": { "$ref": "#/definitions/dates" },
+    "deadlines": {
+      "oneOf": [
+        { "$ref": "#/definitions/dateSet" },
+        {
+          "type": "object",
+          "properties": {
+            "paper":    { "$ref": "#/definitions/dateSet" },
+            "poster":   { "$ref": "#/definitions/dateSet" },
+            "workshop": { "$ref": "#/definitions/dateSet" },
+            "tutorial": { "$ref": "#/definitions/dateSet" },
+            "wip":      { "$ref": "#/definitions/dateSet" }
+          },
+          "additionalProperties": false
+        }
+      ]
+    },
+    "location": {
+      "type": "object",
+      "properties": {
+        "country": { "type": "string" },
+        "state":   { "type": "string" },
+        "city":    { "type": "string" },
+        "venue":   { "type": "string" },
+      }
+    },
+    "homepage": {
+      "type": "string",
+      "format": "url"
+    },
+    "colocated": { "type": "string" }
+  },
+  "additionalProperties": false,
+  "required": [
+    "year"
+  ]
+}
+
+const ajv = new Ajv({ allErrors: true, verbose: true });
+const validate = ajv.compile(schema)
 
 const db = new loki()
 
@@ -20,7 +106,14 @@ fs.readdirSync(db_dir).forEach(cname => {
         const ypath = path.join(cpath, yname)
         console.log("loading call: " + ypath)
         const data = fs.readFileSync(ypath)
-        const doc = yaml.load(data)
+        const doc = yaml.load(data, {
+          schema: yaml.JSON_SCHEMA
+        })
+
+        if (!validate(doc)) {
+          console.log(validate.errors);
+          assert(false);
+        }
 
         // normalize deadlines
         //   1. convert bare date to { submit: date }
