310bd57a364089d9cc1c1792f3886fed0a60644c
Noah Watkins <noahwatkins@gmail.com>
2017-11-09 14:15:19 -0800

    parser: normalize date sets
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- parser.js
+++ parser.js
@@ -44,17 +44,6 @@ const schema = {
       "type": "integer"
     },
     "dates": { "$ref": "#/definitions/dates" },
-    "deadlines": {
-      "type": "object",
-      "properties": {
-        "paper":    { "$ref": "#/definitions/dateSet" },
-        "poster":   { "$ref": "#/definitions/dateSet" },
-        "workshop": { "$ref": "#/definitions/dateSet" },
-        "tutorial": { "$ref": "#/definitions/dateSet" },
-        "wip":      { "$ref": "#/definitions/dateSet" }
-      },
-      "additionalProperties": false
-    },
     "location": {
       "type": "object",
       "properties": {
@@ -69,6 +58,22 @@ const schema = {
       "format": "url"
     },
     "colocated": { "type": "string" }
+  },
+  "patternProperties": {
+    "^deadlines[0-9]*$": {
+      "normalizeDeadlinePhases": true,
+      "type": "object",
+      "properties": {
+        "poster":   { "$ref": "#/definitions/dateSet" },
+        "workshop": { "$ref": "#/definitions/dateSet" },
+        "tutorial": { "$ref": "#/definitions/dateSet" },
+        "wip":      { "$ref": "#/definitions/dateSet" }
+      },
+      "patternProperties": {
+        "^paper[0-9]*$":    { "$ref": "#/definitions/dateSet" },
+      },
+      "additionalProperties": false
+    },
   },
   "additionalProperties": false,
   "required": [
@@ -98,6 +103,52 @@ ajv.addKeyword('normalizeDateSet', {
     if (data.constructor === Array) {
       parentData[parentDataProperty] = {"submit": data}
     }
+  }
+})
+
+ajv.addKeyword('normalizeDeadlinePhases', {
+  modifying: true,
+  schema: false,
+  valid: true,
+  validate: function(data, dataPath, parentData, parentDataProperty) {
+    var result = {}
+    var normalize = []
+    for (const prop in data) {
+      const res = /^([a-z]+)\d+$/.exec(prop);
+      if (res === null) {
+        // wrap in array of length 1
+        result[prop] = [data[prop]];
+      } else {
+        const type = res[1]; // e.g. paper, workshop
+        if (!(type in result)) {
+          result[type] = [prop];
+        } else {
+          result[type].push(prop);
+        }
+        normalize.push(type)
+      }
+    }
+    // sort the keys we'll replace
+    for (const i in normalize) {
+      const key = normalize[i];
+      assert(result[key].length > 1);
+      result[key].sort(function(a, b) {
+        const am = /^[a-z]+([0-9]+)$/.exec(a)
+        const bm = /^[a-z]+([0-9]+)$/.exec(b)
+        return parseInt(am[1]) - parseInt(bm[1]);
+      })
+    }
+    // do the replacements
+    const update = {}
+    for (const i in normalize) {
+      const key = normalize[i];
+      const tmp = result[key].map(function(key2) {
+        return data[key2];
+      })
+      update[key] = tmp;
+    }
+    Object.assign(result, update)
+    parentData[parentDataProperty] = result;
   }
 })
 
